1373228f5e203c55760931214f147725b2b78fce
EfimowEgor <egor.efimowA@yandex.ru>
2023-05-28 17:09:40 +0500

    Старый файл с моделями удалён (переименован)

--- models.py
+++
@@ -1,9 +0,0 @@
-from keras.layers import Dense, LSTM, Flatten, Activation, BatchNormalization, Dropout
-from keras.models import Sequential
-from catboost import CatBoostClassifier
-from xgboost import XGBClassifier
-from lightgbm import LGBMClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, StackingClassifier, BaggingClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
