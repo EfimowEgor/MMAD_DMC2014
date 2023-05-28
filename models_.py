3c74231761eeef582008e8386078a8f5fd301ed2
EfimowEgor <egor.efimowA@yandex.ru>
2023-05-28 17:10:29 +0500

    Определение обучаемых моделей

---
+++ models_.py
@@ -0,0 +1,16 @@
+from keras.layers import Dense, LSTM, Flatten, Activation, BatchNormalization, Dropout
+from keras.models import Sequential
+from catboost import CatBoostClassifier
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, StackingClassifier, BaggingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def XGBoost():
+    print('asdasd')
+    print(1)
+
+def CATBoost():
+    print(1)
